efc0fe161d3f14323ee5da083b596c68bf6f1e9a
Razvan Deaconesu <student@mjolnir.labs.cs.pub.ro>
2015-07-27 12:54:17 +0300

    refactorizat partial

---
+++ halftone.h
@@ -0,0 +1,26 @@
+
+   /***********************************************
+   *
+   *   file halftone.h
+   *
+   *   Functions: This file contains
+   *      main
+   *
+   *   Purpose:
+   *      This file contains the main calling
+   *      routine in an image addition and subtraction
+   *      program.
+   *
+    *
+   *   Modifications:
+   *      1 April 1992 - created
+   *     10 August 1998 - modified to work on entire
+   *         images at one time.
+   *     18 September 1998 - modified to work with 
+   *           all I O routines in imageio.c.
+   *	   Modified Date: 27 Iuly 2015 
+	 *		 Author: Mihu Andrei Cristian
+   *		 Scoala de vara Thales
+   *************************************************/
+#include "mtypes.h"
+#include "cips.h"
--- invert.c
+++ invert.c
@@ -27,75 +27,75 @@
    *           images at once.
    *      19 September 1998 - modified to work with 
    *           all I O routines in imageio.c.
+   *	   Modified Date: 27 Iuly 2015 
+   *		 Author: Mihu Andrei Cristian
+   *		 Scoala de vara Thales
    *
    ***********************************************/
 
-#include "cips.h"
 
+#include "invert.h"
 
-
-
-main(argc, argv)
-   int argc;
-   char *argv[];
+void main(int32_t argc, char_t *argv[])
 {
-   char     name1[80], name2[80];
-   char     *cc;
-   int      a, b; 
-   int      ok = 0;
-   long     length, width;
-   short    **the_image;
-   struct tiff_header_struct image_header;
-   struct bmpfileheader      bmp_file_header;
-   struct bitmapheader       bmheader;
+	char_t name1[80], name2[80];
+	char_t *cc;
+	int32_t a, b, ok = 0; 
+	uint32_t length, width;
+	int16_t **the_image;
+	struct tiff_header_struct image_header;
+	struct bmpfileheader bmp_file_header;
+	struct bitmapheader  bmheader;
 
-   if(argc != 3){
-      printf("\nusage: invert in-file out-file\n");
-      exit(1);
-   }
+	if(argc != 3){
+		printf("\nusage: invert in-file out-file\n");
+		exit(1);
+	}
 
-   strcpy(name1, argv[1]);
-   strcpy(name2, argv[2]);
+	strcpy(name1, argv[1]);
+	strcpy(name2, argv[2]);
 
-   get_image_size(name1, &length, &width);
-  
-   the_image = allocate_image_array(length, width);
+	get_image_size(name1, &length, &width);
 
-   cc = strstr(argv[1], ".tif");
-   if(cc != NULL){  /* create a tif */
-      ok = 1;
-      image_header.lsb            = 1;
-      image_header.bits_per_pixel = 8;
-      image_header.image_length   = length;
-      image_header.image_width    = width;
-      image_header.strip_offset   = 1000;
-      create_allocate_tiff_file(argv[2], 
-                                &image_header);
-   }  /* ends tif */
+	the_image = allocate_image_array(length, width);
+
+	cc = strstr(argv[1], ".tif");
+	if(cc != NULL){  /* create a tif */
+		ok = 1;
+		image_header.lsb            = 1;
+		image_header.bits_per_pixel = 8;
+		image_header.image_length   = length;
+		image_header.image_width    = width;
+		image_header.strip_offset   = 1000;
+		create_allocate_tiff_file(argv[2], 
+		                          &image_header);
+	}  /* ends tif */
 
    cc = strstr(argv[1], ".bmp");
-   if(cc != NULL){  /* create a bmp */
-      ok = 1;
-      bmheader.height = length;
-      bmheader.width  = width;
-      create_allocate_bmp_file(argv[2], 
-                               &bmp_file_header, 
-                               &bmheader);
-   }  /* ends tif */
+	if(cc != NULL){  /* create a bmp */
+		ok = 1;
+		bmheader.height = length;
+		bmheader.width  = width;
+		create_allocate_bmp_file(argv[2], 
+		                         &bmp_file_header, 
+		                         &bmheader);
+	}  /* ends tif */
 
-   if(ok == 0){
-      printf("\nERROR input file neither tiff nor bmp");
-      exit(0);
-   }
+	if(ok == 0){
+		printf("\nERROR input file neither tiff nor bmp");
+		exit(0);
+	}
 
-   read_image_array(name1, the_image);
+	read_image_array(name1, the_image);
 
-   for(a=0; a<length; a++)
-      for(b=0; b<width; b++)
-         the_image[a][b] = GRAY_LEVELS-the_image[a][b];
+	for(a=0; a<length; a++) {
+		for(b=0; b<width; b++) {
+		   the_image[a][b] = GRAY_LEVELS-the_image[a][b];
+		}
+	}
+	write_image_array(name2, the_image);
 
-   write_image_array(name2, the_image);
-
-   free_image_array(the_image, length);
+	free_image_array(the_image, length);
 
 }  /* ends main */
+
