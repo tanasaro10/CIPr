177f62e17f077e460370bfc3dedc931ec95a0b3e
AlexandraBodirlau <alexandra.bodirlau@gmail.com>
2015-07-28 12:23:38 -0700

    Modified - Second Day

---
+++ mtypes.h
@@ -0,0 +1,17 @@
+#ifndef __MTYPES_H
+#define __MTYPES_H
+
+typedef char			char_t;
+typedef unsigned char 		uint8_t;
+typedef signed char 		sint8_t;
+typedef unsigned short int 	uint16_t;
+typedef signed short int	sint16_t;
+typedef unsigned int 		uint32_t;
+typedef signed int		    sint32_t;
+typedef float 				float32_t;
+typedef double 				double32_t;
+
+
+
+
+#endif
