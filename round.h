68d2203ae33eb96429c7fbaeb8e2e37022ee88ff
pardau <stefan_pardau@yahoo.com>
2015-07-30 11:38:54 +0300

    Create round.h

---
+++ round.h
@@ -0,0 +1,16 @@
+/*
+	Modified date: 27/07/2015 
+	Author: Pardau Stefan  
+	Scoala de Cara - Thales -2015	
+*/
+
+#include "cips.h"
+#include "mtypes.h"
+#include "imageio.h"
+
+#define ARGV_ARGUMENT1 1
+#define ARGV_ARGUMENT2 2
+#define ARGV_ARGUMENT3 3
+#define ARGV_ARGUMENT4 4
+#define ARGV_ARGUMENT5 5
+#define ARGV_ARGUMENT6 6
