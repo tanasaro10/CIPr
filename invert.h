41cdf3e826df5a72a7b3359837dba4668bd07e15
mihuandreicristian <mihu.andrei.cristian@gmail.com>
2015-07-30 12:09:18 +0300

    Update invert.h

--- invert.h
+++ invert.h
@@ -1,7 +1,7 @@
 
    /***********************************************
    *
-   *   file invert.c
+   *   file invert.h
    *
    *   Functions: This file contains
    *      main
@@ -33,77 +33,15 @@
    *
    ***********************************************/
 
+#include "cips.h"
+#include "mtypes.h"
 
-#include "invert.h"
-#include "imageio.h"
+enum eTag{
 
-uint16_t main(sint16_t argc, char_t *argv[])
-{
-	char_t name1[80], name2[80];
-	char_t *cc;
-	sint32_t a, b, ok = 0; 
-	uint32_t length, width;
-	sint16_t **the_image;
-	tiff_header_struct image_header;
-	bmpfileheader bmp_file_header;
-	bitmapheader  bmheader;
-	errFlag = eReturnOK;
-
-	if(the_image == NULL) {
-	errFlag = eImageNULL;
-	}
-
-	if(argc != 3){
-		printf("\nusage: invert in-file out-file\n");
-		errFlag = eNotStuffArg;
-	}
-
-	strcpy(name1, argv[1]);
-	strcpy(name2, argv[2]);
-
-	get_image_size(name1, &length, &width);
-
-	the_image = allocate_image_array(length, width);
-
-	cc = strstr(argv[1], ".tif");
-	if(cc != NULL){  /* create a tif */
-		ok = 1;
-		image_header.lsb            = 1;
-		image_header.bits_per_pixel = 8;
-		image_header.image_length   = length;
-		image_header.image_width    = width;
-		image_header.strip_offset   = 1000;
-		create_allocate_tiff_file(argv[2], 
-		                          &image_header);
-	}  /* ends tif */
-
-   cc = strstr(argv[1], ".bmp");
-	if(cc != NULL){  /* create a bmp */
-		ok = 1;
-		bmheader.height = length;
-		bmheader.width  = width;
-		create_allocate_bmp_file(argv[2], 
-		                         &bmp_file_header, 
-		                         &bmheader);
-	}  /* ends tif */
-
-if(errFlag == eReturnOK) {
-
-	if(ok == 0){
-		printf("\nERROR input file neither tiff nor bmp");
-		errFlag = eOKisNotValab;
-	}
-
-	read_image_array(name1, the_image);
-
-	for(a=0; a<length; a++) {
-		for(b=0; b<width; b++) {
-		   the_image[a][b] = GRAY_LEVELS-the_image[a][b];
-		}
-	}
-	write_image_array(name2, the_image);
-
-	free_image_array(the_image, length);
-}
-	return errFlag;
-}  /* ends main */
+	eReturnOK=0,
+	eImageNULL=1,
+	eNotStuffArg=2,
+	eOKisNotValab=3
+	
+}error;
+enum eTag errFlag;
