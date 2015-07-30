8a6b7de0cb608a585b9d84c0b3d294ec975aee9c
mihuandreicristian <mihu.andrei.cristian@gmail.com>
2015-07-30 12:06:02 +0300

    Update halftone.c

--- halftone.c
+++ halftone.c
@@ -1,4 +1,5 @@
 
+  
    /***********************************************
    *
    *   file halftone.c
@@ -19,20 +20,20 @@
    *     18 September 1998 - modified to work with 
    *           all I O routines in imageio.c.
    *	   Modified Date: 27 Iuly 2015 
-	 *		 Author: Mihu Andrei Cristian
+   *		 Author: Mihu Andrei Cristian
    *		 Scoala de vara Thales
    *************************************************/
 
 #include "halftone.h"
 
-void main(uint16_t argc, char_t *argv[])
-{
+uint16_t main(uint16_t argc, char_t *argv[]) {
+
 	char_t  in_name[MAX_NAME_LENGTH];
 	char_t  out_name[MAX_NAME_LENGTH];
-	uint16_t i;
 	uint32_t height, width;
-	uint16_t **the_image, **out_image;
-	uint16_t threshold;
+	sint16_t **the_image, **out_image;
+	uint8_t threshold;
+	errFlag = eReturnOK;
 
    
       /******************************************
@@ -41,14 +42,17 @@ void main(uint16_t argc, char_t *argv[])
       *
       ******************************************/
 
-	if(argc != 4){
-	printf("\nusage: halftone input-image output-image threshold");
-	exit(0);
-	}
+	if(argc != 4) {
 
-	strcpy(in_name,  argv[1]);
-	strcpy(out_name, argv[2]);
-	threshold = atoi(argv[3]);
+		(void)printf("\nusage: halftone input-image output-image threshold");
+		errFlag = eNotSuffArg;
+	}
+	
+	if(errFlag == eReturnOK) {
+
+		strcpy(in_name,  argv[1]);
+		strcpy(out_name, argv[2]);
+		threshold = (uint8_t)atoi(argv[3]);
    
       /******************************************
       *
@@ -60,21 +64,26 @@ void main(uint16_t argc, char_t *argv[])
       *
       ******************************************/
 
-	if(does_not_exist(in_name)){
-		printf("\nERROR input file %s does not exist",in_name);
-		printf("\n      ");
-		printf("usage: histeq input-image output-image");
-		exit(0);
-	}  /* ends if does_not_exist */
+		if(does_not_exist(in_name)) { 
 
-	create_image_file(in_name, out_name);
-	get_image_size(in_name, &height, &width);
-	the_image = allocate_image_array(height, width);
-	out_image = allocate_image_array(height, width);
-	read_image_array(in_name, the_image);
-	half_tone(the_image, out_image,threshold, 200, 0, height, width);
-	write_image_array(out_name, out_image);
-	free_image_array(the_image, height);
-	free_image_array(out_image, height);
+			printf("\nERROR input file %s does not exist",in_name);
+			printf("\n      ");
+			printf("usage: histeq input-image output-image");
+			errFlag = eNoName;
+		}  /* ends if does_not_exist */
 
-}  /* ends main */
+		if(errFlag == eReturnOK) {
+
+			create_image_file(in_name, out_name);
+			get_image_size(in_name, &height, &width);
+			the_image = allocate_image_array(height, width);
+			out_image = allocate_image_array(height, width);
+			read_image_array(in_name, the_image);
+			half_tone(the_image, out_image,threshold, 200, 0, height, width);
+			write_image_array(out_name, out_image);
+			free_image_array(the_image, height);
+			free_image_array(out_image, height);
+		}
+	}
+	return errFlag;
+		}  /* ends main */
