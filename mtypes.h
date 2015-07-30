2910d709517f5b94e9002a0f504c02511b0b63cb
pardau <stefan_pardau@yahoo.com>
2015-07-30 11:41:45 +0300

    Create mtypes.h

---
+++ mtypes.h
@@ -0,0 +1,11 @@
+ #ifndef __MTYPES_H
+ #define __MTYPES_H
+ 
+#include <stdint.h>
+ 
+typedef signed char         sint8_t;
+typedef signed short int    sint16_t;
+typedef signed int          sint32_t;
+typedef char                char_t;
+ 
+ #endif
